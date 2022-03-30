27d2e8e971dc29cf175c9bc0d174898d40f7aacd
CatalanCabbage <45961072+CatalanCabbage@users.noreply.github.com>
2022-03-30 23:20:32 +0530

    remove example specs file, add FF and Safari to test suite

--- playwright.config.js
+++ playwright.config.js
@@ -49,21 +49,21 @@ const config = {
       use: {
         ...devices['Desktop Chrome'],
       },
-    }
+    },
 
-    // {
-    //   name: 'firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
 
     /* Test against mobile viewports. */
     // {
